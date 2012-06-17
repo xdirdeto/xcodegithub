7ec7ccc917cc3b6ed3469b1bfc9fbc938288d3cc
xiaodong <xiaodong.yu@irdeto.com>
2012-06-17 23:48:57 +0200

    build002

---
+++ XcodeGithub/Build002Cell.m
@@ -0,0 +1,29 @@
+//
+//  Build002Cell.m
+//  XcodeGithub
+//
+//  Created by Xiaodong Yu on 17/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Build002Cell.h"
+
+@implementation Build002Cell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
